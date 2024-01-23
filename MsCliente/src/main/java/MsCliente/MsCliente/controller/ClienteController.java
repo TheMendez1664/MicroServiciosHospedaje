cfb6dbe819a04b97a082aade29754b0dd5068c35
YersonTineo56 <yeileon1410@gmail.com>
2024-01-23 14:14:31 -0500

    rrrr

--- MsCliente/src/main/java/MsCliente/MsCliente/controller/ClienteController.java
+++ MsCliente/src/main/java/MsCliente/MsCliente/controller/ClienteController.java
@@ -1,10 +1,13 @@
 package MsCliente.MsCliente.controller;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +16,19 @@ import MsCliente.MsCliente.dto.ClienteDTO;
 import MsCliente.MsCliente.model.Cliente;
 import MsCliente.MsCliente.services.ClienteService;
 
+
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
     private final ClienteService clienteService;
+    private final MessageSource messageSource;
     private final Logger logger = LoggerFactory.getLogger(ClienteController.class);
 
-    public ClienteController(ClienteService clienteService) {
+    @Autowired
+    public ClienteController(ClienteService clienteService, MessageSource messageSource) {
         this.clienteService = clienteService;
+        this.messageSource = messageSource;
     }
 
     @GetMapping
@@ -33,7 +40,8 @@ public class ClienteController {
                     .collect(Collectors.toList());
             return new ResponseEntity<>(clienteDTOs, HttpStatus.OK);
         } catch (Exception e) {
-            logger.error("Error al obtener todos los clientes", e);
+            String errorMessage = messageSource.getMessage("error.getting.all.clients", null, Locale.getDefault());
+            logger.error(errorMessage, e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -46,10 +54,13 @@ public class ClienteController {
                 ClienteDTO clienteDTO = convertToDTO(cliente);
                 return new ResponseEntity<>(clienteDTO, HttpStatus.OK);
             } else {
+                String errorMessage = messageSource.getMessage("error.getting.client.by.id", new Object[]{id}, Locale.getDefault());
+                logger.error(errorMessage);
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
-            logger.error("Error al obtener el cliente con ID: " + id, e);
+            String errorMessage = messageSource.getMessage("error.getting.client.by.id", new Object[]{id}, Locale.getDefault());
+            logger.error(errorMessage, e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -62,7 +73,8 @@ public class ClienteController {
             ClienteDTO creadoDTO = convertToDTO(clienteCreado);
             return new ResponseEntity<>(creadoDTO, HttpStatus.CREATED);
         } catch (Exception e) {
-            logger.error("Error al crear un nuevo cliente", e);
+            String errorMessage = messageSource.getMessage("error.creating.new.client", null, Locale.getDefault());
+            logger.error(errorMessage, e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -75,10 +87,13 @@ public class ClienteController {
                 ClienteDTO actualizadoDTO = convertToDTO(clienteActualizado);
                 return new ResponseEntity<>(actualizadoDTO, HttpStatus.OK);
             } else {
+                String errorMessage = messageSource.getMessage("error.updating.client", new Object[]{id}, Locale.getDefault());
+                logger.error(errorMessage);
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
-            logger.error("Error al actualizar el cliente con ID: " + id, e);
+            String errorMessage = messageSource.getMessage("error.updating.client", new Object[]{id}, Locale.getDefault());
+            logger.error(errorMessage, e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -89,7 +104,8 @@ public class ClienteController {
             clienteService.deleteCliente(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
-            logger.error("Error al eliminar el cliente con ID: " + id, e);
+            String errorMessage = messageSource.getMessage("error.deleting.client", new Object[]{id}, Locale.getDefault());
+            logger.error(errorMessage, e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
